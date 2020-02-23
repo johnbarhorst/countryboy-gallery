0aace7553f49c981716be103fcf57b5e3c8c1dc4
John Barhorst <crashdsinc@comcast.net>
2020-02-23 14:41:35 -0600

    horizontal scrolling a go go

--- client/src/Components/Gallery.js
+++ client/src/Components/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState, Children } from 'react';
+import React, { useState, Children, useEffect } from 'react';
 import styled from 'styled-components';
 
 const GalleryDisplay = styled.div`
@@ -10,22 +10,31 @@ const GalleryDisplay = styled.div`
   position: relative;
 `;
 
-const SlideButton = styled.button`
+const GalleryContent = styled.div`
+  transform: translateX(-${props => props.translate}px);
+  transition: transform ease-out ${props => props.transition}s;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+`;
+
+const BackButton = styled.button`
+  z-index: 1000;
   position: absolute;
   left: 0;
   height: 100%;
   width: 50px;
   opacity: 0;
   background: #333;
-  content: '<';
+  border: none;
   :hover {
-    opacity: .5;
+    opacity: .3;
   }
 `;
 
-const RightButton = styled(SlideButton)`
+const ForwardButton = styled(BackButton)`
   left: ${props => props.w - 50}px;
-  content: '>';
 `;
 
 const Slide = styled.div`
@@ -41,23 +50,35 @@ const GallerySlide = ({ element }) => {
   )
 }
 
-const Gallery = ({ children, interval, width = '500', height = '300' }) => {
+const Gallery = ({ children, interval, width = '400', height = '500' }) => {
   const [currentSlide, setCurrentSlide] = useState(1);
+  const [translate, setTranslate] = useState(0);
+  const [transition, setTransition] = useState(.3);
   const [nextSlide, setNextSlide] = useState(2);
   const [prevSlide, setPrevSlide] = useState(0);
   const [slideArray, setSlideArray] = useState([]);
   const childrenArray = Children.toArray(children);
 
 
-  const handleClick = (e) => {
-    console.log(e.target);
+  const handleBack = () => {
+    setCurrentSlide(currentSlide - 1);
   }
 
+  const handleForward = () => {
+    setCurrentSlide(currentSlide + 1);
+  }
+
+  useEffect(() => {
+    setTranslate(currentSlide * width);
+  }, [currentSlide])
+
   return (
-    <GalleryDisplay w={width} h={height}>
-      <SlideButton />
-      {childrenArray.map((el, i) => (<GallerySlide key={i} element={el} />))}
-      <RightButton w={width} />
+    <GalleryDisplay w={width} h={height} >
+      <BackButton onClick={() => handleBack()} />
+      <GalleryContent translate={translate} transition={transition}>
+        {childrenArray.map((el, i) => (<GallerySlide key={i} element={el} />))}
+      </GalleryContent>
+      <ForwardButton w={width} onClick={() => handleForward()} />
     </GalleryDisplay>
   )
 }
